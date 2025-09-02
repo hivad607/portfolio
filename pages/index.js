2c4f4a77d7205c5c4f2e0e9d3f6a31fa42fc6674
hivad607 <vahidmazari117@gmail.com>
2025-09-02 17:25:33 +0330

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 to-pink-500">
+      <h1 className="text-5xl font-bold text-white">سلام 👋 من یک طراح هستم</h1>
+    </div>
+  );
+}
